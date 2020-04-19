0ef9b80c74604b5707788b06d96a18e070a9fa2d
Avirup-301294 <avirupmondal1994@gmail.com>
2020-04-19 02:17:36 +0000

    initial

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
-   console.log("The YelpCamp Server Has Started!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
