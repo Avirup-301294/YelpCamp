d0b755eab07e3e9c17cd03920fb9f8d9ae5866a8
root <avirup.mondal@crio-users.in>
2022-06-29 10:52:13 +0530

    Add more routes - show, new, create

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const PORT = 3000;
 
@@ -22,16 +21,35 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+// Route - Index
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
 });
+
+// Route - New
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// Route - Post
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// Route - Show
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Serving on port ${PORT}`);
